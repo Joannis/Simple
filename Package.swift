4efa387b5281ee8c0cd359eecc83fd87919b6917
Joannis Orlandos <joannis@orlandos.nl>
2016-05-16 00:56:34 +0200

    Fixed some more dependency issues

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Simple",
     dependencies: [
-                      .Package(url: "https://github.com/PlanTeam/MongoKitten.git", majorVersion: 0, minor: 10),
+                      .Package(url: "https://github.com/PlanTeam/MongoKitten.git", majorVersion: 0, minor: 9),
                       .Package(url: "https://github.com/LoganWright/Genome.git", Version(3,0,0)),
                       .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 8),
                       .Package(url: "https://github.com/Zewo/Reflection.git", majorVersion: 0, minor: 1)
